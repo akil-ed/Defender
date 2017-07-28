9bf930491815585f81fc4baef8b53add57d80bad
Akil <akil@innov8labs.in>
2017-07-28 21:08:13 +0530

    Init

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float speed,minX,minY,maxX,maxY;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (0.1f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+		transform.Translate (Vector3.up * speed * Time.deltaTime);
+
+		if (transform.position.x < minX || transform.position.x > maxX || transform.position.y < minY || transform.position.y > maxY)
+			speed *= -1;
+	}
+}
---
+++ Assets/_Scripts/CamTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTest : MonoBehaviour {
+	public float speed;
+	public float rotateConstant;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey (KeyCode.Mouse0)){
+		//  Quaternion targetRotation = Quaternion.Euler (0, transform.rotation.eulerAngles.y + (MouseHelper.mouseDelta.x),0);
+		//	transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, speed*Time.deltaTime);
+			transform.rotation = Quaternion.Euler (0, transform.rotation.eulerAngles.y + (MouseHelper.mouseDelta.x/rotateConstant),0);
+			}
+	}
+}
